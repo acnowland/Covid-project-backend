6e978059f4cea7933c132b5931e823feb9f8e662
Adam Nowland <acnowland@gmail.com>
2021-03-09 09:42:20 -0700

    backend index change

--- app/controllers/covids_controller.rb
+++ app/controllers/covids_controller.rb
@@ -2,15 +2,12 @@ class CovidsController < ApplicationController
   def index
     response= RestClient.get("https://api.covidtracking.com/v1/states/current.json")
     result = JSON.parse(response)
-    api_states = result["states"]
-    render json: api_states
+    render json: result
   end
 
   def show
     @state = RestClient.get("https://api.covidtracking.com/v1/states/#{params[:id]}/current.json")
-
     render json: @state
-
   end
 
 
